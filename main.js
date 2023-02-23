53b0b860252e5c794d65977a13aaa1019a0ce8c8
mannybennett <mannybennett99@gmail.com>
2023-02-22 21:48:25 -0500

    almost done

--- main.js
+++ main.js
@@ -2,48 +2,32 @@ window.onload = function() {
   getQuotes()
 }
 
-const dragStart = (e) => {
-  e.dataTransfer.setData('text/plain', e.target.textContent);
-  console.log(e.target.textContent)
-  setTimeout(() => {
-    e.target.classList.add('hide');
-  }, 0);
-}
+const dragStart = (e) => e.dataTransfer.setData('text/plain', e.target.textContent)
 
 const getQuotes = () => {
-  fetch ('https://ron-swanson-quotes.herokuapp.com/v2/quotes/10')
-  .then(res => res.json())
-  .then(quotes => {
-    displayQuotes(quotes)
-  })
-  .then(() => {
-    const quoteText = document.querySelector('.quoteText')
-    quoteText.addEventListener('dragstart', dragStart)
-  })
+  fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes/10')
+    .then(res => res.json())
+    .then(quotes => displayQuotes(quotes))
 }
 
 const displayQuotes = (arr) => {
-  console.log(arr)
   const quoteList = document.getElementById('quotes')
   arr.map(quote => {
     const div = document.createElement('div')
     div.setAttribute('draggable', 'true')
     div.className = 'quoteText'
     div.id = quote.split(' ').join('')
-    console.log(div)
     const quoteText = document.createTextNode(`${quote}`)
     div.appendChild(quoteText)
     quoteList.appendChild(div)
+    div.addEventListener('dragstart', dragStart)
   })
 }
 
-//DRAG-DROP FUNCTIONALITY
-
-const boxes = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll('.dropbox');
 
 const dragEnter = (e) => {
   e.preventDefault()
-  console.log(e.target)
   e.target.classList.add('drag-over')
 }
 
@@ -52,19 +36,16 @@ const dragOver = (e) => {
   e.target.classList.add('drag-over')
 }
 
-const dragLeave = (e) => {
-  e.target.classList.remove('drag-over')
-}
+const dragLeave = (e) => e.target.classList.remove('drag-over')
 
 const drop = (e) => {
-  e.target.classList.remove('drag-over')
-  const data = e.dataTransfer.getData('text/plain')
-  const el = document.getElementById(data.split(' ').join(''))
-  console.log(el)
-  const draggable = document.getElementById(el.id)
-  console.log(draggable)
-  e.target.appendChild(draggable)
-  draggable.classList.remove('hide')
+  if (!e.target.childNodes.length) {
+    e.target.classList.remove('drag-over')
+    const data = e.dataTransfer.getData('text/plain')
+    const el = document.getElementById(data.split(' ').join(''))
+    e.target.appendChild(el)
+    pictureDisplay()
+  }
 }
 
 boxes.forEach(box => {
@@ -72,4 +53,14 @@ boxes.forEach(box => {
   box.addEventListener('dragover', dragOver);
   box.addEventListener('dragleave', dragLeave);
   box.addEventListener('drop', drop);
-});
+})
+
+const pictureDisplay = () => {
+  let quoteList = document.getElementById('quotes')
+  if (quoteList.childNodes.length === 0) {
+    const img = document.createElement('img')
+    img.classList.add('image')
+    img.src = "./ronSwanson.jpg"
+    quoteList.appendChild(img)
+  }
+}
